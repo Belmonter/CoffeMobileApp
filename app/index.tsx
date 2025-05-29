1ffab18e2de0c1929926da0dc9bdf02c473a87f5
ogavrilin <ogavrilin@vtb.ru>
2025-05-29 23:32:55 +0300

    Add first screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import Button from "@/components/Button/Button";
+import { Colors, Fonts } from "@/constants/tokens";
+import React, { useCallback } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
+import { Sora_600SemiBold, Sora_400Regular, useFonts } from '@expo-google-fonts/sora';
+
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  const [fontsLoaded, error] = useFonts({Sora_600SemiBold, Sora_400Regular});
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || error) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <>
-      Index
-    </>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <ImageBackground
+        source={require('../assets/images/coffee-background.jpg')}
+        resizeMode="cover"
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>Одно из самых вкусных кофе в городе!</Text>
+        <Text style={styles.subtitle}>
+          Свежие зёрна, настоящая арабика и бережная обжарка
+        </Text>
+        <Button style={styles.button} text="Начать"/>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative', // по умолчанию, но наглядно показываем
+    justifyContent: 'space-between',
+  },
+  content: {
+    flex: 1,
+    padding: 30,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: Fonts.f34,
+    fontFamily: 'Sora_600SemiBold',
+    textAlign: 'center',
+    color: Colors.white,
+  },
+  subtitle: {
+    marginTop: 82,
+    maxWidth: 300,
+    fontSize: Fonts.f14,
+    fontFamily: 'Sora_400Regular',
+    color: Colors.grey,
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: 24,
+    width: '100%',
+  },
+})
 
 export default App;
--- constants/tokens.ts
+++ constants/tokens.ts
@@ -1,1 +1,20 @@
+export const Colors = {
+  black: '#000000',
+  textColor: '#2F2D2C',
+  white: '#ffffff',
+  grey: '#A9A9A9',
+  darkGreen: '#2F4B4E',
+  primary: '#C67C4E',
+  darkPrimary: '#A76237',
+}
 
+export const Radius = {
+  r10: 10,
+  r16: 16,
+}
+
+export const Fonts = {
+  f34: 34,
+  f16: 16,
+  f14: 14,
+}
